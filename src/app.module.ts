2e8ed330c2f29cdb7a2efe4bc71b2723ef7a36b3
Denis Goncharov <imperialeaglex@gmail.com>
2021-06-22 15:41:06 +0300

    fix typeorm sychronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,7 +29,7 @@ const { NODE_ENV } = process.env;
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      synchronize: true,
+      synchronize: NODE_ENV !== 'prod',
       logging: true,
     }),
   ],
